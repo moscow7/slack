43efb097d20e28c9be07ce88d6424c54c1af90a6
moscow7 <mr.mdaler@gmail.com>
2020-05-21 05:34:44 +0200

    for slack

---
+++ src/day_1/Login.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("sonthing new second part");
+
+        //Slack users can login successfully
+    }
+}
