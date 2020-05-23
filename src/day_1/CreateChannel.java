3f800784df6a197be303891b96de49100dbb4c1a
moscow7 <62070914+moscow7@users.noreply.github.com>
2020-05-23 02:54:41 +0200

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,5 +3,12 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User is able to creat channels");
+ 
+ String s="a";
+ if (s.=contains("a")){
+ System.out.println("contains");
+ } else{
+ System.out.println("not");
+         }     
     }
 }
