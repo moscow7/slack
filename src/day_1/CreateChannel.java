0ce73fd34f5ea11115e68461a7b36664bbf3eeb1
moscow7 <mr.mdaler@gmail.com>
2020-05-23 03:02:33 +0200

    update

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,5 +3,14 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User is able to creat channels");
+
+        int x =0; //changed from IntelliJ
+        if (x==0){
+            System.out.println("equal");
+        }
+
+
+
+
     }
 }
